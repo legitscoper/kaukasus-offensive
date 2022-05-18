824e27daf4e1784ce8387eab786e0788ea2cfb1b
Hubert Marek <polskimacerator@gmail.com>
2022-05-19 00:31:42 +0200

    Add map with movable marker for debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 // It contains all main elements of website, header and animated routes which change the content of the page.
 
 import React from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 import Helmet from "react-helmet";
 import "./style.css";
@@ -9,9 +10,36 @@ import "./style.css";
 import Header from "./components/Header";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 
+//import { fromEvent } from "rxjs";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
+// variables
+
+const client = new W3CWebSocket(
+  "wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self"
+);
+
+// end variables
+
+
+
 class App extends React.Component {
+  UNSAFE_componentWillMount() { // executed when component is going to be mounted
+    /*
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+      var object = JSON.parse(message.data);
+      console.log("PLAIN: " + message.data);
+      console.log("JSON: " + object.xd);
+    };
+    */
+    // END WEBSOCKET
+  }
+  
   render() {
     return (
       <>
@@ -26,6 +54,5 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
