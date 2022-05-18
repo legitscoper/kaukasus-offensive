d6e618a19c3429e540339665cfd36844e84c4e7a
Hubert Marek <polskimacerator@gmail.com>
2022-05-19 00:32:23 +0200

    Map with movable marker

--- frontend/src/components/SmallMap.js
+++ frontend/src/components/SmallMap.js
@@ -2,35 +2,101 @@
 
 import React from "react";
 
-import GameMap from "../img/gimp_map.jpg";
+import GameMap from "../img/gimp_map2.jpg";
 
-import L from "leaflet";
-import { Marker, Popup, MapContainer, ImageOverlay } from "react-leaflet";
+import { fromEvent } from "rxjs";
+
+import L, { marker } from "leaflet";
+import { MapContainer, ImageOverlay, useMap, Marker } from "react-leaflet";
 import { CRS } from "leaflet";
 
 import "../style.css";
 
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import MapMarker from "./MapMarker";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
+  iconAnchor: [12, 41],
+  popupAnchor: [0, -34],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const bounds = new L.LatLngBounds([0, 0], [3365, 4578]);
+const bounds = new L.LatLngBounds([0, 0], [3590, 4510]);
 
-const position = [1000, 1000];
+var position = [1000, 1000];
 
+const zoom = 13;
+
+var mapArray = [];
+
+const positionMap = [
+  [0, 0],
+  [100, 100],
+];
+
+fromEvent(document, "click").subscribe(() => {
+  console.log("Clicked!");
+  
+});
 
 class SmallMap extends React.Component {
+  getMapIntervalID;
+
+  state = {
+    // stores all data received from api
+    data: [],
+  };
+
+  componentDidMount() {
+    this.getMapData();
+  }
+  getMapData = () => {
+    console.log("GET");
+    fetch("http://localhost:8000/api/get_map_data", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          data: data,
+        });
+        this.mapArray = data;
+        this.getMapIntervalID = setTimeout(this.getMapData.bind(this), 60000);
+      });
+  };
+
+  componentWillUnmount() {
+    clearTimeout(this.getMapIntervalID);
+  }
+
+  
+  
   render() {
+    
+
+    function MyComponent() {
+      const map = useMap();
+      console.log("map center:", map.getCenter());
+      position = map.getCenter();
+      
+      //console.log(position.lat);
+      return null;
+    }
+    /*const markerxd = L.marker([50.5, 30.5], {
+      draggable: true
+    });
+    markerxd.addTo(map);*/
     return (
-      <div style={{ width: "100%", height: "90vh" }}>
+      <div style={{ width: "100%", height: "90vh", background: "black" }}>
+        <p style={{ color: "white" }}>
+          latitude: {position.lat}, longitude: {position.lng}{" "}
+        </p>
         {
-          //<img src={GameMap} alt="Game Map" width={"1400px"}/>
+          //<p>{this.state.data}</p>
         }
         <MapContainer
           className="map"
@@ -40,18 +106,23 @@ class SmallMap extends React.Component {
           zoom={-2}
           scrollWheelZoom={false}
           minZoom={-2}
-          maxZoom={2}
+          maxZoom={3}
         >
+          <MyComponent />
           <ImageOverlay
             url={GameMap}
             bounds={bounds}
             zIndex={10}
           ></ImageOverlay>
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker 
+            id = {"mid"}
+            draggable={true} 
+            position={position}
+          >
           </Marker>
+          {mapArray.map(function (object, i) {
+            return <MapMarker key={i} position={positionMap[i]} />;
+          })}
         </MapContainer>
       </div>
     );
