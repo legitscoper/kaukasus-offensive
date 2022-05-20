8adc759a413531ca1c14a83bda2ff5cf376b5b5a
Hubert Marek <polskimacerator@gmail.com>
2022-05-21 00:51:30 +0200

    Add Slot component

---
+++ frontend/src/components/Slot.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Slot extends React.Component {
+    render() {
+        return (
+            <div> 
+                - free slot -
+                </div>
+        );
+    }
+}
+
+export default Slot;
