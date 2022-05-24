210d449848a7094559aa523e9d56582d03c0b1c9
Hubert Marek <polskimacerator@gmail.com>
2022-05-24 18:48:12 +0200

    Remove moved socket connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 // It contains all main elements of website, header and animated routes which change the content of the page.
 
 import React from "react";
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+
 
 import Helmet from "react-helmet";
 import "./style.css";
@@ -14,34 +14,14 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-// variables
+/*
+TODO:
+- Instead of using multiple websockets, make data transfer in RxJS
+- Maybe instead of making 2 websockets in backend, make a proxy?
 
-var clientID = Date.now();
-const client = new W3CWebSocket(
-  //"wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self",
-  "ws://localhost:7777" ,
-);
-
-
-// end variables
+*/
 
 class App extends React.Component {
-  UNSAFE_componentWillMount() {
-    // executed when component is going to be mounted
-    console.log("Initialising websocket");
-    client.onopen = () => {
-      console.log("WebSocket Client Connected");
-    };
-    client.onmessage = (message) => {
-      console.log(message);
-      var object = JSON.parse(message.data);
-      console.log("PLAIN: " + message.data);
-      console.log("JSON: " + object.xd);
-    };
-
-    // END WEBSOCKET
-  }
-
   render() {
     return (
       <>
